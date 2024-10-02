00b00bf83c05fcadef8ec03117155f562049dcdd
Chris <christophe.da.cruz.tic@gmail.com>
2024-10-02 15:38:39 +0200

    script bad usb

--- ScreenChanger/script.ps1
+++ ScreenChanger/script.ps1
@@ -1,17 +1,16 @@
-# Télécharger l'image de fond d'écran depuis GitHub
-#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MagikarpLv13/bad_usb_payloads/master/wallpaper.jpg" -OutFile "$env:USERPROFILE\Pictures\wallpaper.jpg"
+# Définir le chemin de l'image de fond d'écran
+$wallpaperPath = "$env:USERPROFILE\Pictures\wallpaper.jpg"
+
+# Télécharger l'image de fond d'écran depuis GitHub et la place dans le dossier Images
+Invoke-WebRequest -Uri "https://github.com/MagikarpLv13/bad_usb_payloads/raw/master/ScreenChanger/wallpaper.jpg" -OutFile $wallpaperPath
 
 # Change le fond d'écran de l'utilisateur
-$wallpaperPath = "$env:USERPROFILE\Pictures\wallpaper.jpg"  # Spécifiez l'emplacement de l'image
-
-
-    # Définir le fond d'écran
-    Add-Type -TypeDefinition @"
-        using System;
-        using System.Runtime.InteropServices;
-        public class Wallpaper {
-            [DllImport("user32.dll", CharSet = CharSet.Auto)]
-            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-        }
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
 "@
-    [Wallpaper]::SystemParametersInfo(0x0014, 0, $wallpaperPath, 0x0001)
+[Wallpaper]::SystemParametersInfo(0x0014, 0, $wallpaperPath, 0x0001)
